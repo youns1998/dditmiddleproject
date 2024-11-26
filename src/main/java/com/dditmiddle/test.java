9e9c0e966b7bbf38ab25af90500b5431f84abc5a
PC-22 <PC-22@DESKTOP-B4I9AGP>
2024-11-26 19:28:53 +0900

    ㅇㅇㅇ연습중

--- src/main/java/com/dditmiddle/test.java
+++ src/main/java/com/dditmiddle/test.java
@@ -2,11 +2,8 @@ package com.dditmiddle;
 
 public class test {
 	
-<<<<<<< HEAD
-	//수정테스트 입니다2222
-=======
-	// 수정.2
 
->>>>>>> branch 'master' of https://github.com/youns1998/dditmiddleproject.git
+
+///dddrer 다시 연습중 
 
 }
