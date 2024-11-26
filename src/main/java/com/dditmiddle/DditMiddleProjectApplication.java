7c524461e62fd68753e16fbefe8a685fb368870c
Choi <Choi@DESKTOP-UN8BP8O>
2024-11-26 18:21:25 +0900

    first commit

---
+++ src/main/java/com/dditmiddle/DditMiddleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.dditmiddle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DditMiddleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DditMiddleProjectApplication.class, args);
+	}
+
+}
